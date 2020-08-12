2a478f025b0342a172be49fd17a87e8c71ed93e8
Dmitrii <ruslanus866@outlook.com>
2020-08-13 02:47:26 +0800

    implemented firebase utils, included ability to store authenticated users into our firebase data

--- src/components/sign-up/signup.component.js
+++ src/components/sign-up/signup.component.js
@@ -64,7 +64,7 @@ class SignUp extends Component {
             name="displayName"
             value={displayName}
             onChange={this.handleChange}
-            lable="Display Name"
+            label="Display Name"
             required
           />
           <FormInput
@@ -72,7 +72,7 @@ class SignUp extends Component {
             name="email"
             value={email}
             onChange={this.handleChange}
-            lable="Email"
+            label="Email"
             required
           />
           <FormInput
@@ -80,7 +80,7 @@ class SignUp extends Component {
             name="password"
             value={password}
             onChange={this.handleChange}
-            lable="Password"
+            label="Password"
             required
           />
           <FormInput
@@ -88,7 +88,7 @@ class SignUp extends Component {
             name="confirmPassword"
             value={confirmPassword}
             onChange={this.handleChange}
-            lable="Confirm Password"
+            label="Confirm Password"
             required
           />
           <CustomButton type="submit">SIGN UP</CustomButton>
